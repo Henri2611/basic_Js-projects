131240692a05ca59a0cce91647a543337709bd5f
henri ouma <147258392+Henri2611@users.noreply.github.com>
2025-02-28 10:57:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,28 @@ const displayContacts = () => {
     });
   }
 };
+const add = ({ name, phone, email }) => {
+  contacts.push({ name, phone, email });
+  console.log("contact added successfully");
+};
+
+const deleteContacts = (info) => {
+  let flag = false;
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].name === info) {
+      const permission = prompt(
+        "Do you really want to delete this contact (y/n):"
+      );
+      if (permission === "y") {
+        const deletedContact = contacts.splice(i, 1);
+        console.log(`${deletedContact[0].name} is deleted successfully`);
+        flag = true;
+        break;
+      }
+    }
+  }
+  if (!flag) {
+    console.log("contact not found");
+  }
+
 

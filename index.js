c91933465cd408e9c2145acd745431e1a11c36e6
henri ouma <147258392+Henri2611@users.noreply.github.com>
2025-02-28 10:58:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,5 +36,43 @@ const deleteContacts = (info) => {
   if (!flag) {
     console.log("contact not found");
   }
+ displayContacts();
+};
 
+const userInput = () => {
+  while (true) {
+    const user = prompt(
+      "press + to add new contact, '-' to delete  contact,  'show' to display the contact list and 'exit' to quit: "
+    );
+
+    if (user === "+") {
+      const newName = prompt("Enter the name: ");
+      const newPhone = prompt("Enter the phone: ");
+      const newEmail = prompt("Enter the email: ");
+
+      if (newName && newPhone && newEmail) {
+        add({ name: newName, phone: newPhone, email: newEmail });
+        displayContacts();
+      } else {
+        console.log("All fields (name, phone, email) are required.");
+      }
+    } else if (user === "show") {
+      displayContacts();
+    } else if (user === "-") {
+      const info = prompt("enter the name  of the contact to delete: ");
+      if (info) {
+        deleteContacts(info);
+      }
+    } else if (user === "exit") {
+      const confirmExit = prompt("Do you really want to quit? (y/n):");
+      if (confirmExit === "y") {
+        console.log("Exiting...");
+        break;
+      }
+    } else {
+      console.log("Invalid command. Please try again.");
+    }
+  }
+};
+userInput();
 
